7dfe23cf17613322164cf76d0c25d49f0a32bacd
Tyler Barnes <tylerdbarnes@gmail.com>
2018-11-08 17:30:23 -0800

    upload previews to the wp install

---
+++ gpress-core/uploadPreviews.js
@@ -0,0 +1,19 @@
+var FormData = require("form-data");
+var fs = require("fs");
+
+var form = new FormData();
+form.append("apikey", "yep");
+form.append("previews", fs.createReadStream("./templates-previews.zip"));
+
+form.submit(
+  "http://gatsbywpmamp.test/wp-content/themes/gatsbypress-admin/webhooks/receivePreviews.php",
+  function(err, res) {
+    if (res.statusCode !== 200) {
+      console.error("Preview Files not uploaded. Check your settings.");
+    } else {
+      console.log("Preview files uploaded successfully");
+    }
+
+    res.resume();
+  }
+);

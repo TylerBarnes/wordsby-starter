71b6d2c08deefed35376711505cf98d13a9fdcd2
Tyler <tyler@known.design>
2019-02-17 15:10:40 -0800

    remove missing fields

--- src/templates/index.js
+++ src/templates/index.js
@@ -21,17 +21,6 @@ export const CollectionQuery = graphql`
     wordsbyCollections(ID: { eq: $id }) {
       post_title
       post_content
-      acf {
-          test {
-            url {
-              childImageSharp {
-                fluid {
-                  originalImg
-                }
-              }
-            }
-          }
-        }
     }
   }
 `;
